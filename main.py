f24b9197a31cdefe78a1cd0c1642eec5e0cff9b0
Warleysonjw <119734069+Warleysonjw@users.noreply.github.com>
2022-12-05 21:50:59 -0300

    adicionar o vosk

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ r = sr.Recognizer()
 
 #Abrir um microfone
 with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
     while True:
        audio = r.listen(source) # Definemicrofone como fonte de áudio
     
