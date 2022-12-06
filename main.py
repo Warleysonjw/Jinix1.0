da32cbe5d9222b66eb0d890d13a370ad6353c8bf
Warleysonjw <119734069+Warleysonjw@users.noreply.github.com>
2022-12-05 21:02:50 -0300

    codigo

---
+++ main.py
@@ -0,0 +1,13 @@
+# Our main file.
+
+import speech_recognition as sr
+
+# Criar um reconhecedor
+r = sr.Recognizer()
+
+#Abrir um microfone
+with sr.Microphone() as source:
+    while True:
+       audio = r.listen(source) # Definemicrofone como fonte de áudio
+    
+       print(r.recognize_google(audio,language='pt'))
